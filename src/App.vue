33201e69ed8084cbe2e11176d674f6bc97108f44
vitodursopettersson <vitodurso98@gmail.com>
2024-02-15 19:19:57 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import AppMain from './components/AppMain.vue';
+import axios from 'axios';
+export default {
+  name: 'Boolfolio',
+  data() {
+    return {
+      apiUrl: 'http://127.0.0.1:8000/api/projects',
+    }
+  },
+  components: {
+    AppMain,
+  }
+}
+</script>
+
+<template>
+  <AppMain />
+</template>
+
+<style scoped></style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,20 @@
+<script>
+
+export default {
+    name: 'AppMain',
+}
+
+</script>
+
+<template>
+    <main>
+
+    </main>
+</template>
+
+<style scoped lang="scss">
+main {
+    height: 100vh;
+    background-color: black;
+}
+</style>
